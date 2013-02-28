d42aaa3d730ba5e3a0c1c0339e69d6c80156c697
Andrew Smallbone <andrew@rocketnumbernine.com>
2013-02-28 11:20:55 +0200

    added helsinki-bustimes.py

--- README.md
+++ README.md
@@ -17,3 +17,14 @@ Displays some weather info for given location using xpath with lxml, example out
 	low: -1C, high: 2C
 	PM Rain/Snow Showers
 	sun: 7:22-5:43
+
+
+**helsinki-bustimes.py**
+
+Given a list of Helsinki bus stop codes, routes, and the time it takes you to walk to each, it will display the departure times so you can decide which to walk to.
+
+	now: 11:15
+	S 11:23 11:38 11:54
+	R 11:37 11:57 12:17
+
+Uses xpath and the lxml API.  This should work for buses/trains but has only been tested with bus stops - there's a huge amount of functionality at [http://developer.reittiopas.fi/pages/en/home.php].
---
+++ helsinki-bustimes.py
@@ -0,0 +1,44 @@
+#! /usr/bin/python
+
+# helsinki-bustimes.py
+# by Andrew Smallbone <andrew@rocketnumbernine.com>.
+# Use freely.
+
+# Prints the next bus times for the specified bus stops and route that leave after
+# the given number of minutes (shortname, busstop code, route, minutes)
+
+# So if you live near two bus stops one takes 5 minutes to walk to, the other 10
+# minutes the following would show the next busses to depart from each so you can
+# decide which to walk to.
+stops = [('S', 'V5139', '2', 5), ('R', 'V5128', '2', 10)]
+# Uses the Helsinki HSL/HRT Reittiopas API which requires a 
+# (free) username/password: from: http://developer.reittiopas.fi/pages/en/home.php
+# check the examples and geolocation API to get busstop codes.
+username = 'xx'
+password = 'xx'
+
+
+# uses lxml: installed with 'pip install lxml' or 'easy_install lxml'
+from lxml import etree
+import re
+
+import datetime
+from datetime import timedelta
+now = datetime.datetime.now()
+
+url = 'http://api.reittiopas.fi/hsl/prod/?format=xml&user={0}&pass={1}&request=stop&code={2}&time={3.hour:02}{3.minute:02}&time_limit=120'
+
+# returns a list of times ("hh:mm") of the next buses leaving the specified 
+# bus stop, on the given line, after the specified minutes from now
+# http://developer.reittiopas.fi/pages/en/http-get-interface-version-2.php#stop
+def get_departures(stop, line, after):
+    departure_time = now+timedelta(minutes=after)
+    doc = etree.parse(url.format(username, password, stop, departure_time))
+    return [re.sub('(..)$', ':\\1', departure.findtext('time')) # return  "hh:mm" instead of "hhmm"
+        for departure in doc.xpath('/response/node/departures/node')
+        if (departure.findtext('code').rsplit()[1] == line)]
+
+print "now: {0.hour}:{0.minute:02}".format(now)
+for stop in stops:
+    print stop[0] + " " + " ".join(get_departures(stop[1], stop[2], stop[3])[:3])
+
